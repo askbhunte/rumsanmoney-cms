884787a38dbe67bbf4ef50cd48088275c65e99d4
Prithvi Maharjan <maharjanprithvi@gmail.com>
2020-08-31 16:40:19 +0545

    compare page added

--- routes/pages.routes.js
+++ routes/pages.routes.js
@@ -4,4 +4,9 @@ router.get('/', async (req, res) => {
   res.render('index', { title: 'Welcome to Chino Loan Website' });
 });
 
+router.get('/compare', async (req, res) => {
+  res.render('compare', { title: 'Welcome to Chino Loan Website' });
+});
+
+
 module.exports = router;
