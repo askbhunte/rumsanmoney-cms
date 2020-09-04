d0a12623124017258d86dfdab0deb30987caf741
Prithvi Maharjan <maharjanprithvi@gmail.com>
2020-09-04 22:17:24 +0545

    sent loan data for comparison from homepage to comparepage

--- routes/pages.routes.js
+++ routes/pages.routes.js
@@ -6,17 +6,19 @@ router.get("/", async (req, res) => {
 });
 
 router.get("/compare", async (req, res, next) => {
-  // try {
-  //   const bankOne = req.query.bank_one || null;
-  //   const bankTwo = req.query.bank_two || null;
-  //   const bankThree = req.query.bank_three || null;
-  //   const data = await Controller.compareBanks(bankOne, bankTwo, bankThree);
-  //   res.json(data);
-  //   // res.render('compare', { title: 'Welcome to Chino Loan Website', data });
-  // } catch (e) {
-  //   next(e);
-  // }
-  res.render("compare");
+  try {
+    const bankOne = req.query.bank_one || null;
+    const bankTwo = req.query.bank_two || null;
+    const bankThree = req.query.bank_three || null;
+    console.log('bankOne,bankTwo,bank_three',bankOne,bankTwo,bankThree);
+    const data = await Controller.compareBanks(bankOne, bankTwo, bankThree);
+    console.log('data sent to reder pages.routes',data);
+    //res.json(data);
+    res.render("compare", { title: 'Welcome to Chino Loan Website', data });
+  } catch (e) {
+    next(e);
+  }
+  //res.render("compare");
 });
 
 module.exports = router;
