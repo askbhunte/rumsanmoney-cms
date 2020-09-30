e164c3f3519f29e4b6f07c7360b555c693edd833
Prithvi Maharjan <maharjanprithvi@gmail.com>
2020-09-30 18:05:23 +0545

    CRUD for category

--- modules/category/category.model.js
+++ modules/category/category.model.js
@@ -1,21 +1,27 @@
 const mongoose = require('mongoose');
 
+const { ObjectId } = mongoose.Schema;
+
 const Schema = mongoose.Schema(
   {
-    name: {
-      type: String,
-      required: true,
-    },
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    slug: { type: String, required: true },
+    is_active: { type: Boolean, required: true },
+    created_at: { type: Date, required: true },
+    updated_at: { type: Date, required: true },
+    created_by: { type: ObjectId, ref: 'Users' },
+    updated_by: { type: ObjectId, ref: 'Users' },
   },
   {
     collection: 'categories',
     toObject: {
-        virtuals: true,
+      virtuals: true,
     },
     toJson: {
       virtuals: true,
     },
-  },    
+  },
 );
 
 module.exports = mongoose.model('Categories', Schema);
