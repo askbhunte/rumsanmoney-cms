1f6730fa88e74f1b6d21f4d6ae12529da8251278
Prithvi Maharjan <maharjanprithvi@gmail.com>
2020-09-30 17:58:26 +0545

    CRUD for bank and product

--- modules/bank/bank.model.js
+++ modules/bank/bank.model.js
@@ -1,21 +1,69 @@
+// const { ObjectID, ObjectId } = require('mongodb');
 const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema(
+const { ObjectId } = mongoose.Schema;
+
+const BankSchema = mongoose.Schema(
   {
     name: {
       type: String,
       required: true,
     },
+    head_office: {
+      type: String,
+      required: true,
+    },
+    contact_number: {
+      type: [],
+      required: true,
+    },
+    primary_contact: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    logo_url: {
+      type: String,
+      required: true,
+    },
+    products: [{ type: ObjectId, ref: 'Products' }],
+    website: {
+      type: String,
+      required: true,
+    },
+    is_active: {
+      type: Boolean,
+      required: true,
+    },
+    created_at: {
+      type: Date,
+      required: true,
+    },
+    updated_at: {
+      type: Date,
+      required: true,
+    },
+    created_by: {
+      type: ObjectId,
+      ref: 'Products',
+    },
+    updated_by: {
+      type: ObjectId,
+      ref: 'Users',
+    },
   },
   {
     collection: 'banks',
     toObject: {
-        virtuals: true,
+      virtuals: true,
     },
     toJson: {
       virtuals: true,
     },
-  },    
+  },
 );
 
-module.exports = mongoose.model('Banks', Schema);
+module.exports = mongoose.model('Banks', BankSchema);
--- modules/product/product.model.js
+++ modules/product/product.model.js
@@ -1,21 +1,30 @@
 const mongoose = require('mongoose');
 
+const { ObjectId } = mongoose.Schema;
 const Schema = mongoose.Schema(
   {
-    name: {
-      type: String,
-      required: true,
-    },
+    name: { type: String, required: true },
+    bank_id: { type: ObjectId, ref: 'Banks' },
+    description: { type: String, required: true },
+    category: [{ type: ObjectId, ref: 'Categories' }],
+    loan_type: { type: String, required: true },
+    interest_rate: { type: Number, required: true },
+    base_rate: { type: Number, required: true },
+    is_active: { type: Boolean, required: true },
+    created_at: { type: Date, required: true },
+    updated_at: { type: Date, required: true },
+    created_by: { type: ObjectId, ref: 'Users' },
+    updated_by: { type: ObjectId, ref: 'Users' },
   },
   {
     collection: 'products',
     toObject: {
-        virtuals: true,
+      virtuals: true,
     },
     toJson: {
       virtuals: true,
     },
-  },    
+  },
 );
 
 module.exports = mongoose.model('Products', Schema);
