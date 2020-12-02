33ec198cf9ad265d9f352177a4b6522faffba5b7
rakimsth <raktim@rumsan.com>
2020-12-02 11:41:47 +0545

    adding extra info in the category model

--- modules/category/category.model.js
+++ modules/category/category.model.js
@@ -8,6 +8,7 @@ const Schema = mongoose.Schema(
     icon: { type: String, required: true },
     description: { type: String },
     required_docs: { type: String },
+    extras: { type: String },
     is_active: { type: Boolean, required: true, default: true },
   },
   {
