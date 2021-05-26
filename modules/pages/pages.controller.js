f01e2c39ed635dae77b311c6a524c66ecde2bca0
Rumsan Associates <depenti.karki@rumsan.com>
2021-05-26 11:15:09 +0545

    all crud operation

--- modules/pages/pages.controller.js
+++ modules/pages/pages.controller.js
@@ -3,6 +3,27 @@ const { DataUtils } = require('../../utils');
 const slugify = require("slugify");
 
 class Controller {
+  async list({
+    start, limit, name,
+  }) {
+    const query = [];
+    if (name) {
+      query.push({
+        $match: {
+          name: new RegExp(name, 'gi'),
+        },
+      });
+    }
+
+
+    return DataUtils.paging({
+      start,
+      limit,
+      sort: { created_at: 1 },
+      model: Model,
+      query,
+    });
+  }
 
   add(payload) {
     let _slug = slugify(payload.name, {
@@ -13,7 +34,33 @@ class Controller {
     payload.slug = _slug;
     return Model.create(payload);
   }
-}
 
+  findById(id) {
+    return Model.findById(id);
+  }
+
+  findBySlug(slug) {
+    return Model.findOne({ slug });
+  }
+
+  update(id, payload) {
+    if (payload.name) {
+      delete payload.slug;
+      payload.slug = payload.name
+        .toLowerCase()
+        .replace(/\s+/g, '-') // Replace spaces with -
+        .replace(/[^\w\-]+/g, '') // Remove all non-word chars
+        .replace(/\-\-+/g, '-') // Replace multiple - with single -
+        .replace(/^-+/, '') // Trim - from start of text
+        .replace(/-+$/, '');
+    }
+    return Model.findByIdAndUpdate(id, payload);
+  }
+
+  remove(id) {
+    return Model.findByIdAndRemove(id);
+  }
+
+}
 
 module.exports = new Controller();
