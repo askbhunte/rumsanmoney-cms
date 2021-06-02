649dcbae9c51b8972ea6d559d909959be09ea678
Prithvi Maharjan <maharjanprithvi@gmail.com>
2021-06-02 15:58:56 +0545

    content fix in pages

--- src/modules/pages/detail/DetailsForm.js
+++ src/modules/pages/detail/DetailsForm.js
@@ -10,176 +10,173 @@ import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import MyUploadAdapter from "../../../services/MyUploader";
 
 import {
-    Card,
-    CardBody,
-    CardTitle,
-    CardHeader,
-    Row,
-    Col,
-    Form,
-    FormGroup,
-    Label,
-    Input,
-    Button,
-    InputGroup,
+  Card,
+  CardBody,
+  CardTitle,
+  CardHeader,
+  Row,
+  Col,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+  InputGroup,
 } from "reactstrap";
 
 import { PagesContext } from "../../../contexts/PagesContext";
 import Loading from "../../global/Loading";
 
 export default function DetailsForm(props) {
-    const [extraContent, setExtraContent] = useState("");
-    const custom_config = {
-        extraPlugins: [MyCustomUploadAdapterPlugin],
-        height: "400px",
-        toolbar: {
-            items: [
-                "heading",
-                "|",
-                "bold",
-                "italic",
-                "link",
-                "bulletedList",
-                "numberedList",
-                "|",
-                "blockQuote",
-                "insertTable",
-                "|",
-                "imageUpload",
-                "mediaEmbed",
-                "|",
-                "undo",
-                "redo",
-            ],
-        },
-        table: {
-            contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"],
-        },
+  const [extraContent, setExtraContent] = useState("");
+  const custom_config = {
+    extraPlugins: [MyCustomUploadAdapterPlugin],
+    height: "400px",
+    toolbar: {
+      items: [
+        "heading",
+        "|",
+        "bold",
+        "italic",
+        "link",
+        "bulletedList",
+        "numberedList",
+        "|",
+        "blockQuote",
+        "insertTable",
+        "|",
+        "imageUpload",
+        "mediaEmbed",
+        "|",
+        "undo",
+        "redo",
+      ],
+    },
+    table: {
+      contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"],
+    },
+  };
+  //ck editor end
+  function MyCustomUploadAdapterPlugin(editor) {
+    editor.plugins.get("FileRepository").createUploadAdapter = (loader) => {
+      return new MyUploadAdapter(loader);
     };
-    //ck editor end
-    function MyCustomUploadAdapterPlugin(editor) {
-        editor.plugins.get("FileRepository").createUploadAdapter = (loader) => {
-            return new MyUploadAdapter(loader);
-        };
-    }
-    const pageId = props.params.id;
-    const { addToast } = useToasts();
-    const [page_details, setPageDetails] = useState(null);
-    const [content, setContent] = useState("");
-    const modules = {
-        toolbar: [
-            [{ header: [1, 2, 3, 4, false] }],
-            ["bold", "italic", "underline"],
-            [{ list: "ordered" }, { list: "bullet" }],
-            ["clean"],
-        ],
-    };
-    const formats = ["bold", "italic", "underline", "list", "bullet"];
+  }
+  const pageId = props.params.id;
+  const { addToast } = useToasts();
+  const [page_details, setPageDetails] = useState(null);
 
-    const {
-        loading,
-        setLoading,
-        resetLoading,
-        getPagesDetails,
-        updatePages,
-    } = useContext(PagesContext);
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, 3, 4, false] }],
+      ["bold", "italic", "underline"],
+      [{ list: "ordered" }, { list: "bullet" }],
+      ["clean"],
+    ],
+  };
+  const formats = ["bold", "italic", "underline", "list", "bullet"];
 
-    const loadPageDetails = () => {
-        getPagesDetails(pageId)
-            .then((d) => {
-                setPageDetails(d);
-                const content = d.content ? d.content : '';
-                setExtraContent(content);
-            })
-            .catch(() => {
-                addToast("Something went wrong!", {
-                    appearance: "error",
-                    autoDismiss: true,
-                });
-            });
-    };
+  const { loading, setLoading, resetLoading, getPagesDetails, updatePages } =
+    useContext(PagesContext);
 
-    const submitUpdate = (e) => {
-        e.preventDefault();
-        let formData = { ...page_details };
-        formData.desc = content;
-        setLoading();
-        updatePages(pageId, formData).then(() => {
-            resetLoading();
-            Swal.fire("Successful!", "Page details updated successfully.", "success")
-                .then((result) => {
-                    if (result.value) {
-                        window.location.href = "/pages";
-                    }
-                })
-                .catch((err) => {
-                    addToast("Something went wrong on server!", {
-                        appearance: "error",
-                        autoDismiss: true,
-                    });
-                    resetLoading();
-                });
+  const loadPageDetails = () => {
+    getPagesDetails(pageId)
+      .then((d) => {
+        setPageDetails(d);
+        const content = d.content ? d.content : "";
+        setExtraContent(content);
+      })
+      .catch(() => {
+        addToast("Something went wrong!", {
+          appearance: "error",
+          autoDismiss: true,
         });
-    };
-    const handleContentChange = async (content) => {
-        setContent(content);
-    };
-    useEffect(loadPageDetails, []);
+      });
+  };
 
-    return (
-        <>
-            <Card>
-                <CardHeader>Add New Pages</CardHeader>
-                <CardBody>
-                    <Form onSubmit={submitUpdate}>
-                        <FormGroup>
-                            <Label for="title">Page Title:</Label>
-                            <Input
-                                type="text"
-                                name="title"
-                                defaultValue={page_details ? page_details.name : ""}
-                                onChange={e => setPageDetails({ ...page_details, name: e.target.value })}
-                                id="title"
-                                placeholder="Enter Title"
-                                required
-                            />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="sub_title">Content:</Label>
-                            <br />
-                            <CKEditor
-                                editor={ClassicEditor}
-                                config={custom_config}
-                                data={extraContent}
-                                onChange={(event, editor) => {
-                                    const data = editor.getData();
-                                    setExtraContent(data);
-                                }}
-                            />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="Status">Status:</Label>
-                            <Input
-                                type="select"
-                                name="status"
-                                placeholder="Status"
-                                className="form-field"
-                                value={page_details ? page_details.status : ""}
-                                onChange={e => setPageDetails({ ...page_details, status: e.target.value })}
-                                required
-                            >
-                                <option value="">-- Select Type --</option>
-                                <option value="PUBLISHED">PUBLISHED</option>
-                                <option value="DRAFT">DRAFT</option>
-                            </Input>
-                        </FormGroup>
-                        <Button type="submit">Submit</Button>
-                        <Link to="/pages" className="btn btn-danger ml-2">
-                            Cancel
-          </Link>
-                    </Form>
-                </CardBody>
-            </Card>
-        </>
-    );
+  const submitUpdate = (e) => {
+    e.preventDefault();
+    let formData = { ...page_details };
+    formData.content = extraContent;
+    setLoading();
+    updatePages(pageId, formData).then(() => {
+      resetLoading();
+      Swal.fire("Successful!", "Page details updated successfully.", "success")
+        .then((result) => {
+          if (result.value) {
+            window.location.href = "/pages";
+          }
+        })
+        .catch((err) => {
+          addToast("Something went wrong on server!", {
+            appearance: "error",
+            autoDismiss: true,
+          });
+          resetLoading();
+        });
+    });
+  };
+
+  useEffect(loadPageDetails, []);
+
+  return (
+    <>
+      <Card>
+        <CardHeader>Add New Pages</CardHeader>
+        <CardBody>
+          <Form onSubmit={submitUpdate}>
+            <FormGroup>
+              <Label for="title">Page Title:</Label>
+              <Input
+                type="text"
+                name="title"
+                defaultValue={page_details ? page_details.name : ""}
+                onChange={(e) =>
+                  setPageDetails({ ...page_details, name: e.target.value })
+                }
+                id="title"
+                placeholder="Enter Title"
+                required
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="sub_title">Content:</Label>
+              <br />
+              <CKEditor
+                editor={ClassicEditor}
+                config={custom_config}
+                data={extraContent}
+                onChange={(event, editor) => {
+                  const data = editor.getData();
+                  setExtraContent(data);
+                }}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="Status">Status:</Label>
+              <Input
+                type="select"
+                name="status"
+                placeholder="Status"
+                className="form-field"
+                value={page_details ? page_details.status : ""}
+                onChange={(e) =>
+                  setPageDetails({ ...page_details, status: e.target.value })
+                }
+                required
+              >
+                <option value="">-- Select Type --</option>
+                <option value="PUBLISHED">PUBLISHED</option>
+                <option value="DRAFT">DRAFT</option>
+              </Input>
+            </FormGroup>
+            <Button type="submit">Submit</Button>
+            <Link to="/pages" className="btn btn-danger ml-2">
+              Cancel
+            </Link>
+          </Form>
+        </CardBody>
+      </Card>
+    </>
+  );
 }
