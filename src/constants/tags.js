d872a56d58d2546b0e66288138e6cafa01a10524
Uttam Raj Giri <uttam.giri@rumsan.com>
2021-08-31 20:56:50 +0545

    adding constant in seperate page

---
+++ src/constants/tags.js
@@ -0,0 +1,8 @@
+module.exports =
+    [
+        { value: 'agegroup_18to25', label: '18 - 25 Age Group' },
+        { value: 'agegroup_25to40', label: '25 - 40 Age Group' },
+        { value: 'agegroup_40to60', label: '40 - 60 Age Group' },
+        { value: 'agegroup_60to80', label: '60 - 80 Age Group' }
+    ]
+    ;
--- src/modules/products/tags.select.js
+++ src/modules/products/tags.select.js
@@ -1,16 +1,11 @@
 import React from 'react'
 import Select from 'react-select'
-
+import options from '../../constants/tags'
 export default function () {
-    const options = [
-        { value: 'agegroup_18to25', label: '18 - 25 Age Group' },
-        { value: 'agegroup_25to40', label: '25 - 40 Age Group' },
-        { value: 'agegroup_40to60', label: '40 - 60 Age Group' },
-    ]
     return (
         <Select isMulti
             options={options}
-            placeholder={'Select tags'} />
+            placeholder={'Select tags '} />
     )
 }
 
