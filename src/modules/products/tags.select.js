69f2b1f3c65294eb94767c6c7c677590f67cc0dc
Uttam Raj Giri <uttam.giri@rumsan.com>
2021-09-01 19:35:33 +0545

    fixes

--- src/modules/products/tags.select.js
+++ src/modules/products/tags.select.js
@@ -1,19 +1,29 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Select from 'react-select'
 import options from '../../constants/tags'
 export default function ({ existingTags, onChange }) {
-    console.log(existingTags);
+    const [selectedTags, setSelectedTags] = useState([]);
     const handleSelect = e => {
         const tags = e.map(
             d => d.value
         )
         onChange(tags);
     }
+    useEffect(() => {
+        if (existingTags) {
+            const finalData = existingTags.map(e => {
+                const x = options.filter(options => options.value === e);
+                return x[0];
+            });
+            setSelectedTags(finalData);
+        }
+    }, [existingTags]);
     return (
         <Select isMulti
             options={options}
+            value={selectedTags}
             onChange={e => handleSelect(e)}
-            placeholder={'Select tags '} />
+            placeholder="Select tags" />
     )
 }
 
