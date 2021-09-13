10bd4a9e61049d0eeebb8674a6b7dc4d3179b969
rakimsth <raktim@rumsan.com>
2021-09-14 00:10:02 +0545

    analytics list/detail page cleanup

--- src/modules/analytics/details/DetailsForm.js
+++ src/modules/analytics/details/DetailsForm.js
@@ -1,223 +1,245 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { Row, Col, Card, CardTitle, CardBody, Table } from 'reactstrap';
-import { Context } from '../core/contexts';
-import Loading from '../../global/Loading';
-import { dateFormatter } from '../../../utils/formatter';
-import { useToasts } from 'react-toast-notifications';
-import Paginate from '../../global/Paginate';
-const DetailForm = props => {
-	const Id = props.params.id;
-	const { addToast } = useToasts();
-	const { listHistory, get, pagination } = useContext(Context);
-	const [detail, setDetail] = useState(null);
-	const [userHistory, setUserHistory] = useState(null);
-	const [current, setCurrent] = useState(0);
+import React, { useState, useEffect, useContext } from "react";
+import { Row, Col, Card, CardTitle, CardBody, Table } from "reactstrap";
+import { Context } from "../core/contexts";
+import Loading from "../../global/Loading";
+import { dateFormatter } from "../../../utils/formatter";
+import { useToasts } from "react-toast-notifications";
+import Paginate from "../../global/Paginate";
+const DetailForm = (props) => {
+  const Id = props.params.id;
+  const { addToast } = useToasts();
+  const { listHistory, get, pagination } = useContext(Context);
+  const [detail, setDetail] = useState(null);
+  const [userHistory, setUserHistory] = useState(null);
+  const [current, setCurrent] = useState(0);
 
-	const handlePagination = current_page => {
-		let _start = current_page * pagination.limit;
-		setCurrent(current_page);
-		return loadList({
-			start: _start,
-			limit: pagination.limit,
-		})
-	}
+  const handlePagination = (current_page) => {
+    let _start = current_page * pagination.limit;
+    setCurrent(current_page);
+    return loadList({
+      start: _start,
+      limit: pagination.limit,
+    });
+  };
 
-	const loadList = (query) => {
-		listHistory(detail._id, query)
-			.then(d => {
-				setUserHistory(d.data);
-			})
-			.catch(() => {
-				addToast('Something went wrong!', {
-					appearance: 'error',
-					autoDismiss: true
-				});
-			});
-	};
+  const loadList = (query) => {
+    listHistory(detail._id, query)
+      .then((d) => {
+        setUserHistory(d.data);
+      })
+      .catch(() => {
+        addToast("Something went wrong!", {
+          appearance: "error",
+          autoDismiss: true,
+        });
+      });
+  };
 
-	const loaddetail = () => {
-		get(Id)
-			.then(d => {
-				setDetail(d);
-			})
-			.catch(() => {
+  const loaddetail = () => {
+    get(Id)
+      .then((d) => {
+        setDetail(d);
+      })
+      .catch(() => {});
+  };
+  const userHistoryList = () => {
+    if (detail) {
+      listHistory(detail._id)
+        .then((d) => {
+          setUserHistory(d.data);
+        })
+        .catch(() => {});
+    }
+  };
+  useEffect(loaddetail, []);
+  useEffect(userHistoryList, [detail]);
 
-			});
-	};
-	const userHistoryList = () => {
-		if (detail) {
-			listHistory(detail._id)
-				.then(d => {
-					setUserHistory(d.data);
-				})
-				.catch(() => {
+  return (
+    <>
+      {detail ? (
+        <div>
+          <Card>
+            <CardTitle className="mb-0 p-3 border-bottom bg-light">
+              <Row>
+                <Col md="8">
+                  <i className="mdi mdi-account mr-2"></i>User Analytics Detail
+                </Col>
+              </Row>
+            </CardTitle>
 
-				});
-		}
-	};
-	useEffect(loaddetail, []);
-	useEffect(userHistoryList, [detail]);
+            <CardBody>
+              <Row>
+                <Col md="12">
+                  <h5>Name</h5>
+                  <div className="mt-2">{detail.user ? detail.user : "-"}</div>
+                </Col>
+              </Row>
+              <Row className="mt-5">
+                <Col md="6">
+                  <h5>Cookie Name</h5>
+                  <div className="mt-2">{detail ? detail.name : "-"}</div>
+                </Col>
+                <Col md="6">
+                  <h5> IP Address</h5>
+                  <div className="mt-2">
+                    {detail && detail.ip ? detail.ip : "-"}
+                  </div>
+                </Col>
+              </Row>
+              <Row className="mt-5">
+                <Col md="6">
+                  <h5>Device</h5>
+                  <div className="mt-2">
+                    {detail && detail.device ? detail.device : "-"}
+                  </div>
+                </Col>
+                <Col md="6">
+                  <h5>Referral</h5>
+                  <div className="mt-2">
+                    {detail && detail.referral ? detail.referral : "-"}
+                  </div>
+                </Col>
+              </Row>
+            </CardBody>
+          </Card>
+          {detail && detail.preference.length > 0 && (
+            <Card>
+              <CardTitle className="mb-0 p-3 border-bottom bg-light">
+                <Row>
+                  <Col md="8">
+                    <i className="mdi mdi-account-star-variant mr-2"></i>User
+                    Preference Detail
+                  </Col>
+                </Row>
+              </CardTitle>
+              {detail.preference.reverse().map((d) => {
+                return (
+                  <CardBody key={d.full_name}>
+                    <Row className="mb-4">
+                      <Col md={12}>
+                        <h5>
+                          Created at:
+                          <span> {d.created_at ? d.created_at : "-"}</span>
+                        </h5>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col md="4">
+                        <h5>Name</h5>
+                        <div className="mt-2">
+                          {d.full_name ? d.full_name : "-"}
+                        </div>
+                      </Col>
+                      <Col md="4">
+                        <h5>Email</h5>
+                        <div className="mt-2">{d.email ? d.email : "-"}</div>
+                      </Col>
+                      <Col md="4">
+                        <h5>Phone Number</h5>
+                        <div className="mt-2">{d.phone ? d.phone : "-"}</div>
+                      </Col>
+                    </Row>
+                    <Row className="mt-5">
+                      <Col md="4">
+                        <h5>Age Group</h5>
+                        <div className="mt-2">
+                          {d.age_group ? d.age_group : "-"}
+                        </div>
+                      </Col>
+                      <Col md="4">
+                        <h5> Occupation</h5>
+                        <div className="mt-2">
+                          {d.occupation ? d.occupation : "-"}
+                        </div>
+                      </Col>
+                      <Col md="4">
+                        <h5> Foreign employment?</h5>
+                        <div className="mt-2">
+                          {d.foreign_employment ? d.foreign_employment : "-"}
+                        </div>
+                      </Col>
+                    </Row>
+                    <Row className="mt-5">
+                      <Col md="4">
+                        <h5>Annual Income</h5>
+                        <div className="mt-2">
+                          {d.annual_income ? d.annual_income : "-"}
+                        </div>
+                      </Col>
+                      <Col md="4">
+                        <h5>Location</h5>
+                        <div className="mt-2">
+                          {d.location ? d.location : "-"}
+                        </div>
+                      </Col>
+                      <Col md="4">
+                        <h5>Gender</h5>
+                        <div className="mt-2">{d.gender ? d.gender : "-"}</div>
+                      </Col>
+                    </Row>
 
-	return (
-		<>
-			{detail ? (
-				<div>
-					<Card>
-						<CardTitle className="mb-0 p-3 border-bottom bg-light">
-							<Row>
-								<Col md="8">
-									<i className="mdi mdi-account mr-2"></i>User Analytics Detail
-								</Col>
-							</Row>
-						</CardTitle>
+                    <Row>
+                      <Col md={12}>
+                        <hr className="mt-5"></hr>
+                      </Col>
+                    </Row>
+                  </CardBody>
+                );
+              })}
+            </Card>
+          )}
+        </div>
+      ) : (
+        <Loading />
+      )}
 
-						<CardBody>
-							<Row>
-								<Col md="12">
-									<h5>Name</h5>
-									<div className="mt-2">{detail.user ? detail.user : '-'}</div>
-								</Col>
-							</Row>
-							<Row className="mt-5">
-								<Col md="6"><h5>Cookie Name</h5>
-									<div className="mt-2">{detail ? detail.name : "-"}</div>
-								</Col>
-								<Col md="6">
-									<h5> IP Address</h5>
-									<div className="mt-2">{detail && detail.ip ? detail.ip : "-"}</div>
-								</Col>
-							</Row>
-							<Row className="mt-5">
-								<Col md="6">
-									<h5>Device</h5>
-									<div className="mt-2">{detail && detail.device ? detail.device : "-"}</div>
-								</Col>
-								<Col md="6">
-									<h5>Referral</h5>
-									<div className="mt-2">{detail && detail.referral ? detail.referral : "-"}</div>
-								</Col>
-							</Row>
-						</CardBody>
-					</Card>
-					<Card>
-						<CardTitle className="mb-0 p-3 border-bottom bg-light">
-							<Row>
-								<Col md="8">
-									<i className="mdi mdi-account-star-variant mr-2"></i>User Preference Detail
-								</Col>
-							</Row>
-						</CardTitle>
-						{detail.preference.reverse().map(d => {
-							return (
-								<CardBody key={d.full_name} >
-
-									<Row className="mb-4">
-										<Col md={12} >
-											<h5>Created at:<span> {d.created_at ? d.created_at : "-"}</span></h5>
-
-										</Col>
-									</Row>
-									<Row>
-										<Col md="4">
-											<h5>Name</h5>
-											<div className="mt-2">{d.full_name ? d.full_name : "-"}</div>
-										</Col>
-										<Col md="4">
-											<h5>Email</h5>
-											<div className="mt-2">{d.email ? d.email : "-"}</div>
-										</Col>
-										<Col md="4">
-											<h5>Phone Number</h5>
-											<div className="mt-2">{d.phone ? d.phone : "-"}</div>
-										</Col>
-									</Row>
-									<Row className="mt-5">
-										<Col md="4"><h5>Age Group</h5>
-											<div className="mt-2">{d.age_group ? d.age_group : "-"}</div>
-										</Col>
-										<Col md="4">
-											<h5> Occupation</h5>
-											<div className="mt-2">{d.occupation ? d.occupation : "-"}</div>
-										</Col>
-										<Col md="4">
-											<h5> Foreign employment?</h5>
-											<div className="mt-2">{d.foreign_employment ? d.foreign_employment : "-"}</div>
-										</Col>
-									</Row>
-									<Row className="mt-5">
-										<Col md="4">
-											<h5>Annual Income</h5>
-											<div className="mt-2">{d.annual_income ? d.annual_income : "-"}</div>
-										</Col>
-										<Col md="4">
-											<h5>Location</h5>
-											<div className="mt-2">{d.location ? d.location : "-"}</div>
-										</Col>
-										<Col md="4">
-											<h5>Gender</h5>
-											<div className="mt-2">{d.gender ? d.gender : "-"}</div>
-										</Col>
-
-									</Row>
-
-									<Row>
-										<Col md={12}>
-											<hr className="mt-5"></hr>
-										</Col>
-									</Row>
-								</CardBody>
-
-							)
-						})
-
-						}
-					</Card>
-
-				</div>
-
-			) : (
-				<Loading />
-			)
-			}
-
-			<Card>
-				<CardTitle className="mb-0 p-3 border-bottom bg-light">
-					<Row>
-						<Col md="4">
-							<i className="mdi mdi-chart-line mr-2"></i>User Analytics
-						</Col>
-					</Row>
-				</CardTitle>
-				<CardBody>
-					<Table className="no-wrap v-middle" responsive>
-						<thead>
-							<tr className="border-0">
-								<th className="border-0">S.N.</th>
-								<th className="border-0">User Actions</th>
-							</tr>
-						</thead>
-						<tbody>
-							{userHistory && userHistory.length ? (
-								userHistory.map((d, i) => {
-									return (
-										<tr key={d._id}>
-											<td>{i + 1}</td>
-											<td>{d && d.data ? d.data : '-'}</td>
-											<td>{d && d.created_at ? dateFormatter(d.created_at, 'lll') : '-'}</td>
-										</tr>
-									);
-								})
-							) : (
-								<tr>
-									<td colSpan={4}>No data available.</td>
-								</tr>
-							)}
-						</tbody>
-					</Table>
-					<Paginate limit={pagination.limit} total={pagination.total} current={current} onChange={handlePagination} />
-				</CardBody>
-			</Card>
-		</>
-	);
+      <Card>
+        <CardTitle className="mb-0 p-3 border-bottom bg-light">
+          <Row>
+            <Col md="4">
+              <i className="mdi mdi-chart-line mr-2"></i>User Analytics
+            </Col>
+          </Row>
+        </CardTitle>
+        <CardBody>
+          <Table className="no-wrap v-middle" responsive>
+            <thead>
+              <tr className="border-0">
+                <th className="border-0">S.N.</th>
+                <th className="border-0">User Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {userHistory && userHistory.length ? (
+                userHistory.map((d, i) => {
+                  return (
+                    <tr key={d._id}>
+                      <td>{i + 1}</td>
+                      <td>{d && d.data ? d.data : "-"}</td>
+                      <td>
+                        {d && d.created_at
+                          ? dateFormatter(d.created_at, "lll")
+                          : "-"}
+                      </td>
+                    </tr>
+                  );
+                })
+              ) : (
+                <tr>
+                  <td colSpan={4}>No data available.</td>
+                </tr>
+              )}
+            </tbody>
+          </Table>
+          <Paginate
+            limit={pagination.limit}
+            total={pagination.total}
+            current={current}
+            onChange={handlePagination}
+          />
+        </CardBody>
+      </Card>
+    </>
+  );
 };
 export default DetailForm;
--- src/modules/analytics/list/ListTable.js
+++ src/modules/analytics/list/ListTable.js
@@ -1,184 +1,218 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { useToasts } from 'react-toast-notifications';
-import { Row, Col, Card, CardTitle, CardBody, Input, Table, CustomInput } from 'reactstrap';
-import { Context } from '../core/contexts';
-import Paginate from '../../global/Paginate';
-import { properCase } from '../../../utils/formatter';
+import React, { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+import { useToasts } from "react-toast-notifications";
+import {
+  Row,
+  Col,
+  Card,
+  CardTitle,
+  CardBody,
+  Input,
+  Table,
+  CustomInput,
+} from "reactstrap";
+import { Context } from "../core/contexts";
+import Paginate from "../../global/Paginate";
+import { properCase } from "../../../utils/formatter";
 
 const List = () => {
-	const { addToast } = useToasts();
-	const [current, setCurrent] = useState(0);
-	const { data, list, pagination } = useContext(Context);
-	const [searchText, setSearchText] = useState('');
-	const [filter, setFilter] = useState({
-		searchPlaceholder: "Enter name...",
-		searchBy: "user",
-	});
+  const { addToast } = useToasts();
+  const [current, setCurrent] = useState(0);
+  const { data, list, pagination } = useContext(Context);
+  const [searchText, setSearchText] = useState("");
+  const [filter, setFilter] = useState({
+    searchPlaceholder: "Enter name...",
+    searchBy: "user",
+  });
 
-	const fetchList = query => {
-		let params = { ...pagination, ...query };
-		list(params)
-			.then()
-			.catch(() => {
-				addToast('Internal server error!', {
-					appearance: 'error',
-					autoDismiss: true
-				});
-			});
-	};
+  const fetchList = (query) => {
+    let params = { ...pagination, ...query };
+    list(params)
+      .then()
+      .catch(() => {
+        addToast("Internal server error!", {
+          appearance: "error",
+          autoDismiss: true,
+        });
+      });
+  };
 
-	const handlePagination = current_page => {
-		let _start = current_page * pagination.limit;
-		setCurrent(current_page);
-		let query = { user: searchText };
-		if (filter.searchBy === searchOptions.COOKIENAME) {
-			query = { cookieName: searchText };
-		}
-		return loadList({
-			start: _start,
-			limit: pagination.limit,
-			...query
-		});
-	};
+  const handlePagination = (current_page) => {
+    let _start = current_page * pagination.limit;
+    setCurrent(current_page);
+    let query = { user: searchText };
+    if (filter.searchBy === searchOptions.COOKIENAME) {
+      query = { cookieName: searchText };
+    }
+    return loadList({
+      start: _start,
+      limit: pagination.limit,
+      ...query,
+    });
+  };
 
-	const searchOptions = {
-		USER: "user",
-		COOKIENAME: "cookieName"
-	};
+  const searchOptions = {
+    USER: "user",
+    COOKIENAME: "cookieName",
+  };
 
-	const handleSortChange = (e) => {
-		let { value } = e.target;
-		return fetchList({ start: 0, limit: pagination.limit, preferenceCheck: value });
-	};
-	const handleFilterChange = (e) => {
-		let { value } = e.target;
-		if (value === searchOptions.USER) {
-			setFilter({
-				searchPlaceholder: "Enter UserName...",
-				searchBy: searchOptions.USER,
-			});
-		}
-		if (value === searchOptions.COOKIENAME) {
-			setFilter({
-				searchPlaceholder: "Enter CookieName...",
-				searchBy: searchOptions.COOKIENAME,
-			});
-		}
-		fetchList({ start: 0, limit: pagination.limit });
-	};
+  const handleSortChange = (e) => {
+    let { value } = e.target;
+    return fetchList({
+      start: 0,
+      limit: pagination.limit,
+      preferenceCheck: value,
+    });
+  };
+  const handleFilterChange = (e) => {
+    let { value } = e.target;
+    if (value === searchOptions.USER) {
+      setFilter({
+        searchPlaceholder: "Enter UserName...",
+        searchBy: searchOptions.USER,
+      });
+    }
+    if (value === searchOptions.COOKIENAME) {
+      setFilter({
+        searchPlaceholder: "Enter CookieName...",
+        searchBy: searchOptions.COOKIENAME,
+      });
+    }
+    fetchList({ start: 0, limit: pagination.limit });
+  };
 
-	const handleSearchInputChange = (e) => {
-		const { value } = e.target;
-		setSearchText(value);
-		if (filter.searchBy === searchOptions.USER) {
-			return fetchList({ start: 0, limit: pagination.limit, user: value });
-		}
-		if (filter.searchBy === searchOptions.COOKIENAME) {
-			return fetchList({ start: 0, limit: pagination.limit, cookieName: value });
-		}
-		fetchList({ start: 0, limit: pagination.limit });
-	};
+  const handleSearchInputChange = (e) => {
+    const { value } = e.target;
+    setSearchText(value);
+    if (filter.searchBy === searchOptions.USER) {
+      return fetchList({ start: 0, limit: pagination.limit, user: value });
+    }
+    if (filter.searchBy === searchOptions.COOKIENAME) {
+      return fetchList({
+        start: 0,
+        limit: pagination.limit,
+        cookieName: value,
+      });
+    }
+    fetchList({ start: 0, limit: pagination.limit });
+  };
 
-	const loadList = query => {
-		if (!query) query = null;
-		list(query)
-			.then()
-			.catch(() => {
-				addToast('Something went wrong!', {
-					appearance: 'error',
-					autoDismiss: true
-				});
-			});
-	};
+  const loadList = (query) => {
+    if (!query) query = null;
+    list(query)
+      .then()
+      .catch(() => {
+        addToast("Something went wrong!", {
+          appearance: "error",
+          autoDismiss: true,
+        });
+      });
+  };
 
+  useEffect(fetchList, []);
 
-
-	useEffect(fetchList, []);
-
-	return (
-		<>
-			<Card>
-				<CardTitle className="mb-0 p-3 border-bottom bg-light">
-					<Row>
-						<Col md="6">
-							<i className="mdi mdi-border-right mr-2"></i>User Analytics
-						</Col>
-						<Col md="2">
-							<CustomInput id="sortCookie" type="select" name="customSelect" onChange={handleSortChange}>
-								<option value="">Filter Cookies by</option>
-								<option value="userPreference">Preference exists?</option>
-								<option value="userHistory">History exists?</option>
-							</CustomInput>
-						</Col>
-						<Col md="4" >
-							<CustomInput
-								type="select"
-								id="exampleCustomSelect"
-								name="customSelect"
-								defaultValue=""
-								style={{ width: "auto" }}
-								onChange={handleFilterChange}
-							>
-								<option value="user">Search By UserName</option>
-								<option value="cookieName">Search By CookieName</option>
-							</CustomInput>
-							<div style={{ display: "inline-flex" }}>
-								<Input
-									placeholder={filter.searchPlaceholder}
-									onChange={handleSearchInputChange}
-									style={{ width: "100%" }}
-								/>
-							</div>
-						</Col>
-
-					</Row>
-				</CardTitle>
-				<CardBody>
-					<Table className="no-wrap v-middle" responsive>
-						<thead>
-							<tr className="border-0">
-								<th className="border-0">Username</th>
-								<th className="border-0">Cookie Name</th>
-								<th className="border-0">IP Address</th>
-								<th className="border-0">Device</th>
-								<th className="border-0">Action</th>
-							</tr>
-						</thead>
-						<tbody>
-							{data.length ? (
-								data.reverse().map(d => {
-									return (
-										<tr key={d._id}>
-											<td>
-												<div className="text-dark">{d.user ? d.user : '-'}</div>
-											</td>
-											<td>
-												<div className="text-dark">{d.name ? d.name : '-'}</div>
-											</td>
-											<td>{d && d.ip ? d.ip : '-'}</td>
-											<td>{d && d.device ? properCase(d.device).substring(0, 42).concat('', '...') : '-'}</td>
-											<td className="blue-grey-text text-darken-4 font-medium">
-												<Link className="btn btn-secondary" to={`/analytic/${d.name}`}>
-													<i className="fa fa-eye"></i>
-												</Link>
-											</td>
-										</tr>
-									);
-								})
-							) : (
-								<tr>
-									<td colSpan={4}>No data available.</td>
-								</tr>
-							)}
-						</tbody>
-					</Table>
-					<Paginate limit={pagination.limit} total={pagination.total} current={current} onChange={handlePagination} />
-				</CardBody>
-			</Card>
-			<br />
-		</>
-	);
+  return (
+    <>
+      <Card>
+        <CardTitle className="mb-0 p-3 border-bottom bg-light">
+          <Row>
+            <Col md="4">
+              <i className="mdi mdi-border-right mr-2"></i>User Analytics
+            </Col>
+            <Col md="8">
+              <Row>
+                <Col md="4">
+                  <CustomInput
+                    id="sortCookie"
+                    type="select"
+                    name="customSelect"
+                    onChange={handleSortChange}
+                  >
+                    <option value="">Filter Cookies by</option>
+                    <option value="userPreference">Preference exists?</option>
+                    <option value="userHistory">History exists?</option>
+                  </CustomInput>
+                </Col>
+                <Col md="4">
+                  <CustomInput
+                    type="select"
+                    id="exampleCustomSelect"
+                    name="customSelect"
+                    onChange={handleFilterChange}
+                  >
+                    <option value="user">Search By UserName</option>
+                    <option value="cookieName">Search By CookieName</option>
+                  </CustomInput>
+                </Col>
+                <Col md="4">
+                  <Input
+                    placeholder={filter.searchPlaceholder}
+                    onChange={handleSearchInputChange}
+                  />
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </CardTitle>
+        <CardBody>
+          <Table className="no-wrap v-middle" responsive>
+            <thead>
+              <tr className="border-0">
+                <th className="border-0">Username</th>
+                <th className="border-0">Cookie Name</th>
+                <th className="border-0">IP Address</th>
+                <th className="border-0">Device</th>
+                <th className="border-0">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.length ? (
+                data.reverse().map((d) => {
+                  return (
+                    <tr key={d._id}>
+                      <td>
+                        <div className="text-dark">{d.user ? d.user : "-"}</div>
+                      </td>
+                      <td>
+                        <div className="text-dark">{d.name ? d.name : "-"}</div>
+                      </td>
+                      <td>{d && d.ip ? d.ip : "-"}</td>
+                      <td>
+                        {d && d.device
+                          ? properCase(d.device)
+                              .substring(0, 42)
+                              .concat("", "...")
+                          : "-"}
+                      </td>
+                      <td className="blue-grey-text text-darken-4 font-medium">
+                        <Link
+                          className="btn btn-secondary"
+                          to={`/analytic/${d.name}`}
+                        >
+                          <i className="fa fa-eye"></i>
+                        </Link>
+                      </td>
+                    </tr>
+                  );
+                })
+              ) : (
+                <tr>
+                  <td colSpan={4}>No data available.</td>
+                </tr>
+              )}
+            </tbody>
+          </Table>
+          <Paginate
+            limit={pagination.limit}
+            total={pagination.total}
+            current={current}
+            onChange={handlePagination}
+          />
+        </CardBody>
+      </Card>
+      <br />
+    </>
+  );
 };
 
 export default List;
