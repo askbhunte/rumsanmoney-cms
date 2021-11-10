a555415ad3d4d6b723c8e6cdcdbe47366c23a314
Prithvi Maharjan <maharjanprithvi@gmail.com>
2021-11-10 18:27:16 +0545

    set has_history to true on history post

--- modules/history/history.routes.api.js
+++ modules/history/history.routes.api.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const Controller = require('./history.controller');
+const CookieController = require('../cookie/cookie.controller');
 
 router.get('/:id', (q, r, n) => {
   const start = q.query.start || 0;
@@ -11,7 +12,11 @@ router.get('/:id', (q, r, n) => {
     .catch(e => n(e));
 });
 
-router.post('/', (q, r, n) => {
+router.post('/', async (q, r, n) => {
+  const cookieDetail = await CookieController.getById(q.body.cookie);
+  if (cookieDetail.has_history === false) {
+    await CookieController.getByIdAndUpdate(q.body.cookie, { has_history: true });
+  }
   Controller.add(q.body)
     .then(d => r.json(d))
     .catch(e => n(e));
