327a9d07ac126b2396f0d57f7bf35301a487f623
Raktim Shrestha <raktim@rumsan.com>
2021-11-12 13:11:44 +0545

    cookie routes cleanup

--- modules/cookie/cookie.routes.api.js
+++ modules/cookie/cookie.routes.api.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
-const { query } = require('express');
 const { v4: uuidv4 } = require('uuid');
 const Controller = require('./cookie.controller');
-const HistoryController = require('../history/history.controller');
 
 router.post('/', (q, r, n) => {
   const payload = q.body;
@@ -17,14 +15,12 @@ router.get('/', (q, r, n) => {
   const limit = q.query.limit || 20;
   const cookieName = q.query.cookieName || null;
   const user = q.query.user || null;
-  // const filterPreferenceUser = !!(q.query && q.query.preferenceCheck === 'userPreference');
   let filter;
   if (q.query && q.query.preferenceCheck === 'userHistory') {
     filter = 'userHistory';
   } else if (q.query && q.query.preferenceCheck === 'userPreference') {
     filter = 'userPreference';
   }
-
   Controller.list({
     start,
     limit,
@@ -59,7 +55,7 @@ router.put('/id/:id', (q, r, n) => {
 router.patch('/:name', (q, r, n) => {
   Controller.updateCookieUserName(q.params.name, q.body.username)
     .then(d => r.json(d))
-    .catch(e => console.log(e));
+    .catch(e => n(e));
 });
 
 router.post('/:name', async (q, r, n) => {
